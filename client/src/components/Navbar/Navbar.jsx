e98ebbb1a4f43b3123b841bb4f9defbd28ffa0b3
vrajclerk <vrajclerk04@gmail.com>
2024-02-21 01:05:40 +0530

    minor

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ function NavbarLg({ name }) {
       <div className='w-full md:flex hidden justify-between shadow items-center py-3 lg:px-16 md:px-12 sm:px-8 px-2'>
         <Link to="/" className='flex gap-1.5 items-center'>
           {/* <img src={logo} className='h-[70px] object-contain' alt="" /> */}
-          <h1 className='font-bold text-xl font-mono'>Tiffin Wala</h1>
+          <h1 className='font-bold text-xl font-mono'>Tiffin Buddy</h1>
         </Link>
         <ul className='gap-4 flex items-center font-semibold '>
           <li><Link to="/provider">Tiffin Providers</Link></li>
@@ -28,7 +28,7 @@ function NavbarSm({ name }) {
       <div className='flex md:hidden px-4 py-2 shadow justify-between'>
         <Link to="/" className='flex items-center'>
           <img src={logo} className='h-[50px] object-contain' alt="" />
-          <h1 className='font-bold text-xl font-mono'>Tiffin Wala</h1>
+          <h1 className='font-bold text-xl font-mono'>Tiffin Buddy</h1>
         </Link>
         <div className='flex items-center gap-3'>
         {name ? <ProfileAvatar name={name} /> : <NavbarMenu />}
--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -43,7 +43,7 @@ function SignInPage() {
       <div className='py-2 sm:px-8 px-2 shadow flex justify-between items-center'>
         <Link to="/" className='flex items-center'>
           {/* <img src={logo} className='h-8 object-contain' alt="" /> */}
-          <h1 className='font-bold text-xl font-mono'>Tiffin Wala</h1>
+          <h1 className='font-bold text-xl font-mono'>Tiffin Buddy</h1>
         </Link>
         <Link to="/" className='flex items-center gap-2'>
           <BiArrowBack />
