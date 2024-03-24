cdd69bd6b9ec2547f9e6359bd91a2fbeb5527bad
vrajclerk <vrajclerk04@gmail.com>
2024-03-24 23:02:30 +0530

    quotes

--- client/src/components/Home/HeroSlider.jsx
+++ client/src/components/Home/HeroSlider.jsx
@@ -5,19 +5,23 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import HeroSliderCard from './HeroSliderCard';
+import s1 from '../../images/thali1.jpeg'
 
 function HeroSlider() {
   const slidesData = [
     {
-      title: "Get Your Meals Delivered from Curated Home Chefs Around You",
+      title: "Get Your Meals from Curated Home Chefs Around You",
       img: "https://alignthoughts.com/wp-content/uploads/2016/10/align-thoughts-indian-thali.jpg"
+      //img  : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKO0imCCGCWjPOK0pNloIoYKU3J1I65jp12GmXbG3pniOrRPdyjvqCorPjIQ&s"
+      
     },
     {
-      title: "Food Is Our Common Ground, Universal Experience",
+      title: "Meal plans made easy, taste guaranteed.",
       img: "https://imgs.search.brave.com/QtZppep4gzh0sfbCVSUaSrHeBSLuMppfQSLjoFPEeDw/rs:fit:1200:628:1/g:ce/aHR0cHM6Ly9pbWdz/dGF0aWNjb250ZW50/LmxiYi5pbi9sYmJu/ZXcvd3AtY29udGVu/dC91cGxvYWRzL3Np/dGVzLzIvMjAxOC8w/NS8yMjIxMzM1OS9I/b21lbHkxLmpwZz93/PTEyMDAmaD02Mjgm/ZmlsbD1ibHVyJmZp/dD1maWxs"
+      
     },
     {
-      title: "Happiness Is The Smell Of Your Favourite Food. Get It Here",
+      title: "Life is uncertain. Order your  favorite meal anytime and anywhere.",
       img: "https://imgs.search.brave.com/dwzMINAc1hsf9-eiM-TkcOrU-SyE3VFc134CVEfE4w8/rs:fit:1200:800:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2RlLzE0/Lzc2L2RlMTQ3NjJl/MTIzOWE1YmEzNTU4/ZGZkYTgwYmFmNzhi/LmpwZw"
     },
   ]
